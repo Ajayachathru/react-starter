a9d83516a21b6bfe42561f66e08e17ef871b1e96
Ajayachathru <ajayachathru.s@lantrasoft.com>
2021-07-01 12:10:56 +0530

    Employee list completed. Parent to child, Child to parent communications handled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { employees } from "./data/employees";
+import {
+    Jumbotron,
+    Container,
+    Form,
+    Table,
+    Button,
+    Spinner,
+} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+const EmployeeRow = ({ employee, onEmployeeDelete }) => {
+    return (
+        <tr>
+            <td>{employee.id}</td>
+            <td>{employee.name}</td>
+            <td>{employee.role}</td>
+            <td>
+                <Button
+                    variant="danger"
+                    onClick={onEmployeeDelete}
+                    value={employee.id}
+                >
+                    Delete
+                </Button>
+            </td>
+        </tr>
+    );
+};
+
+const EmptyRow = ({ colSpan, jsxElement }) => {
+    return (
+        <tr>
+            <td colSpan={colSpan}>{jsxElement}</td>
+        </tr>
+    );
+};
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            employeeFilter: "",
+            employees: [],
+            isEmployeeLoading: true,
+        };
+    }
+
+	componentDidMount() {
+		this.getEmployees();
+	}
+
+    getEmployees = () => {	
+		setTimeout(() => {
+			this.setState({
+				employees,
+				isEmployeeLoading: false,
+			});
+		}, 1500);
+    };
+
+    handleSearchEvent = (evt) => {
+        const filterValue = evt.target.value;
+        let filteredEmployees = [];
+
+        if (filterValue.trim()) {
+            filteredEmployees = this.state.employees.filter(
+                (employee) =>
+                    employee.name.includes(filterValue) ||
+                    employee.id.includes(filterValue)
+            );
+        } else {
+            filteredEmployees = employees; // Reset with default values
+        }
+
+        this.setState({
+            employeeFilter: filterValue,
+            employees: filteredEmployees,
+        });
+    };
+
+    onDeleteEmployee = (evt) => {
+        this.setState({
+            employees: this.state.employees.filter(
+                (employee) => employee.id !== evt.target.value
+            ),
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Jumbotron>
+                    <Container>
+                        <h1>Employees</h1>
+
+                        <Form.Group>
+                            <Form.Control
+                                size="lg"
+                                type="text"
+                                placeholder="Search by name or id"
+                                value={this.state.employeeFilter}
+                                onChange={this.handleSearchEvent}
+                            />
+                        </Form.Group>
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Role</th>
+                                    <th>Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.employees.length ? (
+                                    this.state.employees.map((employee) => {
+                                        return (
+                                            <EmployeeRow
+                                                employee={employee}
+                                                onEmployeeDelete={
+                                                    this.onDeleteEmployee
+                                                }
+                                                key={employee.id}
+                                            />
+                                        );
+                                    })
+                                ) : (
+                                    <EmptyRow
+                                        colSpan={4}
+                                        jsxElement={
+                                            this.state.isEmployeeLoading ? (
+                                                <Spinner animation="border" />
+                                            ) : (
+                                                <span>
+                                                    No matching employees !
+                                                </span>
+                                            )
+                                        }
+                                    />
+                                )}
+                            </tbody>
+                        </Table>
+                    </Container>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/data/employees.js
@@ -0,0 +1,24 @@
+module.exports = {
+    employees: [
+        {
+            id: "LS1229",
+            name: "ajayachathru",
+            role: "Senior systems analyst",
+        },
+        {
+            id: "LS1220",
+            name: "Krishnakumar",
+            role: "Senior systems analyst",
+        },
+        {
+            id: "LS1210",
+            name: "Suresh",
+            role: "systems analyst",
+        },
+        {
+            id: "LS1211",
+            name: "Shyam",
+            role: "Technical Lead",
+        },
+    ],
+};
